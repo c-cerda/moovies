f13d9729c3f5eee113cf10db20af7c8645b1dbbe
c-cerda <karlos.cerda157@gmail.com>
2026-03-23 15:45:36 -0600

    aqui mete la v2

--- js/contact.js
+++
@@ -1,9 +0,0 @@
-// --- FUNCIÓN DE CONTACTO (Archivo separado) ---
-function contactUs() {
-    const email = "feriva00526@gmail.com";
-    const subject = "Duda sobre MOOVIES";
-    const body = "Hola, tengo una duda sobre la página web...";
-    
-    // Abre el cliente de correo predeterminado
-    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-}
